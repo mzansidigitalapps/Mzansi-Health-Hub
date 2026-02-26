744711de1c5f6a430e8e83c1ae866155f9de4796
Mzansi Digital Apps <admin@mzansidigitalapps.co.za>
2026-02-26 08:12:31 +0200

    Add color palette to theme/colors.js

---
+++ theme/colors.js
@@ -0,0 +1,7 @@
+export default {
+  primary: '#0A4D8C',   // Deep Medical Blue
+  secondary: '#1CA7A6', // Soft Teal
+  background: '#FFFFFF',
+  emergency: '#E53935', // Red for SOS button
+  text: '#1F1F1F'
+};
